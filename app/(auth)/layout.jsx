8894df79ad55665bc39aeced03a2b47aa5151cba
Suyash1505 <adv.mks7171@gmail.com>
2025-10-26 16:45:00 +0530

    set up userauth

---
+++ app/(auth)/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AuthLayout = ({childern}) => {
+  return (
+    <div className= "flex justify-centre pt-40">
+        {childern}
+    </div>
+  )
+}
+
+export default AuthLayout
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
+
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -10,18 +13,23 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className}`}>
 
-        {/* HEADER */}
+          {/* HEADER */}
+          <Header/>
 
-        <main className="min-h-screen">
-          {children}
-        </main>
-        
+          {/* MAIN COMPONENT */}
+          <main className="min-h-screen">
+            {children}
+          </main>
+          
 
-        {/* FOOTER */}
-      </body>
-    </html>
+          {/* FOOTER */}
+        </body>
+      </html>
+    </ClerkProvider>
+    
   );
 }
